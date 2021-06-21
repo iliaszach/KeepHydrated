b0b84e47c131e4e6787c68b2ea2cbe20bf54b0de
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-06-21 12:52:55 +0300

    Add project files.

---
+++ KeepHydrated/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepHydrated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Litres(11.8));
+        }
+
+
+
+        public static int Litres(double time)
+        {
+            int a = (int)Math.Floor(time * 0.5);
+            return a;
+        }
+    }
+}
